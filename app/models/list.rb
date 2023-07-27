7d28440105c77e33209909007f9ddb77aa3fb9a3
kyoomaru <138374899+kyoomaru@users.noreply.github.com>
2023-07-27 00:54:24 +0000

    [Finish]sample_app2[chapter2]

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,10 @@
 class List < ApplicationRecord
   has_one_attached :image
-  
+
   validates :title, presence: true
   validates :body, presence: true
+
+  # rails c やるとき限定でコメントアウトすること！終わったら戻してね
   validates :image, presence: true
 end
+
